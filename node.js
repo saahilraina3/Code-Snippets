cd382ab4f34072ec768537b7178f8630ea2cbd86
Sahil Raina <58129963+saahilraina3@users.noreply.github.com>
2022-07-04 21:31:00 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,36 @@
+console.log("Terminal")
+
+
+let firstName = "Mike";
+function userName(){
+console.log("Hello " + firstName );
+}
+
+userName();
+
+
+//---------------------------------------------
+
+//Arrary & Array Index
+
+let nums= [20,11,32,37,49]
+console.log(nums[3]);
+
+console.log(nums.toString());
+
+//Push & Pop with Array
+
+console.log(nums.pop())
+
+
+
+//--------------------------------------------------
+
+//String Addition
+
+let a = "Mike " + 12 + 7;
+
+let b = 12 + 7 + " Mike";
+
+console.log(a);
+console.log(b);
