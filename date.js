01cc9f98a583736b8ba9d42f3a0f7e923a1e612b
Sahil Raina <58129963+saahilraina3@users.noreply.github.com>
2023-03-05 10:54:31 +0000

    container config done

--- date.js
+++ date.js
@@ -1,1 +1,8 @@
-console.log("Saahil");
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+let createdDate = Date.now()
+console.log(createdDate)
